0beae8ab44cc3850523b5344fca577ce57b9b1c2
Beau <jamesbuchanan0528@gmail.com>
2023-01-02 22:37:27 -0600

    Palindromr

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,29 @@
+using PalindromeExercise;
 using System;
 using Xunit;
+using Xunit.Sdk;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("kayak", true)]
+        [InlineData("eve", true)]
+        [InlineData("banana", false)]
+        [InlineData("noon", true)]
+        [InlineData("rotator", true)]
+        [InlineData("racecars", false)]
+        public void IsAPalindromeTest(string word, bool expected)
         {
+            //Arrange
+            var instance = new WordSmith();
 
+            //Act
+            var actual = instance.IsAPalindrome(word);
+
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
